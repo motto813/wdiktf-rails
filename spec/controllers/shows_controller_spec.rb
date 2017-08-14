4df30ceae1e7e629e93b3b931f0cf107fa232aa0
paul <hammerschmidtpaul@gmail.com>
2017-08-14 07:58:21 -0500

    spec for tmdb search now mimics real controller action, makes two http requests. movie.yml saves both http request/responses

--- spec/controllers/shows_controller_spec.rb
+++ spec/controllers/shows_controller_spec.rb
@@ -3,10 +3,14 @@ require 'rails_helper'
 RSpec.describe ShowsController, type: :controller do
   describe 'Test TheMovieDB API' do
     # 254320 is tmdb_id for 'The Lobster'
-    let(:movie_id) { 254320 }
+    let(:movie_name) { "The Lobster" }
 
     it 'returns a movie object with an accessible name from TheMovieDB API' do
       VCR.use_cassette "movie" do
+        search = Tmdb::Search.new
+        search.resource("movie")
+        search.query(movie_name)
+        movie_id = search.fetch[0]["id"]
         movie = Tmdb::Movie.detail(movie_id)
         expect(p movie["original_title"]).to be_a String
       end
